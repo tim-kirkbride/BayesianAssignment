67908e7e098eb14d23e05e43f99c6db4af2e66a8
Arion-Evans <arionevans@gmail.com>
2018-09-27 19:54:15 +1000

    3 different MCMC scripts with different levels of standardisation on the independent and dependent variables

---
+++ MCMC Functions/Jags-Ymet-XmetMulti-Mrobust(Norm-Stand).R
@@ -0,0 +1,301 @@
+# Jags-Ymet-XmetMulti-Mrobust.R 
+# Accompanies the book:
+#  Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition: 
+#  A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
+
+
+
+#===============================================================================
+
+genMCMC = function( data , xName="x" , yName="y" , 
+                    numSavedSteps=10000 , thinSteps=1 , saveName=NULL  ,
+                    runjagsMethod=runjagsMethodDefault , 
+                    nChains=nChainsDefault , xPred = xPred) { 
+  require(runjags)
+  #-----------------------------------------------------------------------------
+  # THE DATA.
+  y = data[,yName]
+  x = as.matrix(data[,xName],ncol=length(xName))
+  # Do some checking that data make sense:
+  if ( any( !is.finite(y) ) ) { stop("All y values must be finite.") }
+  if ( any( !is.finite(x) ) ) { stop("All x values must be finite.") }
+  cat("\nCORRELATION MATRIX OF PREDICTORS:\n ")
+  show( round(cor(x),3) )
+  cat("\n")
+  flush.console()
+  # Specify the data in a list, for later shipment to JAGS:
+  dataList = list(
+    x = x ,
+    y = y ,
+    Nx = dim(x)[2] ,
+    Ntotal = dim(x)[1] ,
+    xPred = xPred, # Data for predictions
+    Npred = dim(xPred)[1]
+  )
+  #-----------------------------------------------------------------------------
+  # THE MODEL.
+  modelString = "
+  # Standardize the data:
+  data {
+  ym <- mean(y)
+  ysd <- sd(y)
+  for ( i in 1:Ntotal ) {
+  zy[i] <- ( y[i] - ym ) / ysd
+  }
+  for ( j in 1:Nx ) {
+  xm[j]  <- mean(x[,j])
+  xsd[j] <-   sd(x[,j])
+  for ( i in 1:Ntotal ) {
+  zx[i,j] <- ( x[i,j] - xm[j] ) / xsd[j]
+  }
+  }
+
+  
+  # Specify the priors for original beta parameters
+  # Prior locations to reflect the expert information
+  mu0 <- ym # Set to overall mean a priori based on the interpretation of constant term in regression
+  mu[1:Nx] <- rep(0,Nx)
+  
+  # Prior variances to reflect the expert information    
+  Var0 <- 1E+6 
+  Var[1:Nx] <- rep(1E+6,Nx)
+
+  
+  # Compute corresponding prior means and variances for the standardised parameters
+  muZ[1:Nx] <-  mu[1:Nx] * xsd[1:Nx] / ysd 
+  
+  muZ0 <- (mu0 + sum( mu[1:Nx] * xm[1:Nx] / xsd[1:Nx] )*ysd - ym) / ysd 
+  
+  # Compute corresponding prior variances and variances for the standardised parameters
+  VarZ[1:Nx] <- Var[1:Nx] * ( xsd[1:Nx]/ ysd )^2
+  VarZ0 <- Var0 / (ysd^2)
+  
+  }
+  # Specify the model for standardized data:
+  model {
+  for ( i in 1:Ntotal ) {
+  zy[i] ~ dnorm( zbeta0 + sum( zbeta[1:Nx] * zx[i,1:Nx] ) , 1/zsigma^2 )
+  }
+  
+  # Priors vague on standardized scale:
+  zbeta0 ~ dnorm( muZ0 , 1/VarZ0 )  
+  for ( j in 1:Nx ) {
+  zbeta[j] ~ dnorm( muZ[j] , 1/VarZ[j] )
+  }
+  zsigma ~ dunif( 1.0E-5 , 5E+1 )
+ 
+  
+  # Transform to original scale:
+  beta[1:Nx] <- ( zbeta[1:Nx] / xsd[1:Nx] )*ysd
+  beta0 <- zbeta0*ysd  + ym - sum( zbeta[1:Nx] * xm[1:Nx] / xsd[1:Nx] )*ysd
+  sigma <- zsigma*ysd
+  
+  # Compute predictions at every step of the MCMC
+  pred[1:Npred] <- beta0 + beta[1] * xPred[1:Npred,1] + beta[2] * xPred[1:Npred,2] + beta[3] * xPred[1:Npred,3] + beta[4] * xPred[1:Npred,4] 
+  + beta[5] * xPred[1:Npred,5]
+  
+  }
+  " # close quote for modelString
+  # Write out modelString to a text file
+  writeLines( modelString , con="TEMPmodelReg.txt" )
+  #-----------------------------------------------------------------------------
+  # INTIALIZE THE CHAINS.
+  # Let JAGS do it...
+  
+  # Must standardize data first...
+  #   lmInfo = lm( zy ~ zx )
+  #   initsList = list(
+  #     beta0 = lmInfo$coef[1] ,   
+  #     beta = lmInfo$coef[-1] ,        
+  #     sigma = sqrt(mean(lmInfo$resid^2)) ,
+  #     nu = 5
+  #   )
+  
+  
+  #-----------------------------------------------------------------------------
+  # RUN THE CHAINS
+  parameters = c( "beta0" ,  "beta" ,  "sigma", 
+                  "zbeta0" , "zbeta" , "zsigma", "pred" )
+  adaptSteps = 1000  # Number of steps to "tune" the samplers
+  burnInSteps = 1000
+  runJagsOut <- run.jags( method=runjagsMethod ,
+                          model="TEMPmodelReg.txt" , 
+                          monitor=parameters , 
+                          data=dataList ,  
+                          #inits=initsList , 
+                          n.chains=nChains ,
+                          adapt=adaptSteps ,
+                          burnin=burnInSteps , 
+                          sample=ceiling(numSavedSteps/nChains) ,
+                          thin=thinSteps ,
+                          summarise=FALSE ,
+                          plots=FALSE )
+  codaSamples = as.mcmc.list( runJagsOut )
+  # resulting codaSamples object has these indices: 
+  #   codaSamples[[ chainIdx ]][ stepIdx , paramIdx ]
+  # Added by Demirhan
+  # if ( !any(is.null(xPred)) ) {
+  #   for ( i in 1:nChains){
+  #     pred = codaSamples[[i]][,"beta0"]
+  #     for ( pName in colnames(xPred) ) {
+  #       pred = pred + codaSamples[[i]][,pName]*as.numeric(xPred[pName])
+  #     }
+  #     codaSamples[[i]]  =cbind(codaSamples[[i]] , as.data.frame(as.matrix(pred)) )
+  #   }
+  #   codaSamples = as.mcmc.list( codaSamples )
+  #   
+  # }
+  
+  if ( !is.null(saveName) ) {
+    save( codaSamples , file=paste(saveName,"Mcmc.Rdata",sep="") )
+  }
+  return( codaSamples )
+} # end function
+
+#===============================================================================
+
+smryMCMC = function(  codaSamples , 
+                      saveName=NULL) {
+  summaryInfo = NULL
+  mcmcMat = as.matrix(codaSamples,chains=TRUE)
+  paramName = colnames(mcmcMat)
+  for ( pName in paramName ) {
+    summaryInfo = rbind( summaryInfo , summarizePost( mcmcMat[,pName] ) )
+  }
+  rownames(summaryInfo) = paramName
+  
+  if ( !is.null(saveName) ) {
+    write.csv( summaryInfo , file=paste(saveName,"SummaryInfo.csv",sep="") )
+  }
+  
+  return( summaryInfo)
+}
+
+#===============================================================================
+
+plotMCMC = function( codaSamples , data , xName="x" , yName="y" ,
+                     showCurve=FALSE ,  pairsPlot=FALSE ,
+                     saveName=NULL , saveType="jpg" ) {
+  # showCurve is TRUE or FALSE and indicates whether the posterior should
+  #   be displayed as a histogram (by default) or by an approximate curve.
+  # pairsPlot is TRUE or FALSE and indicates whether scatterplots of pairs
+  #   of parameters should be displayed.
+  #-----------------------------------------------------------------------------
+  y = data[,yName]
+  x = as.matrix(data[,xName])
+  mcmcMat = as.matrix(codaSamples,chains=TRUE)
+  chainLength = NROW( mcmcMat )
+  zbeta0 = mcmcMat[,"zbeta0"]
+  zbeta  = mcmcMat[,grep("^zbeta$|^zbeta\\[",colnames(mcmcMat))]
+  if ( ncol(x)==1 ) { zbeta = matrix( zbeta , ncol=1 ) }
+  zsigma = mcmcMat[,"zsigma"]
+  beta0 = mcmcMat[,"beta0"]
+  beta  = mcmcMat[,grep("^beta$|^beta\\[",colnames(mcmcMat))]
+  if ( ncol(x)==1 ) { beta = matrix( beta , ncol=1 ) }
+  sigma = mcmcMat[,"sigma"]
+  pred = mcmcMat[,grep("^pred$|^pred\\[",colnames(mcmcMat))]
+    
+  #-----------------------------------------------------------------------------
+  # Compute R^2 for credible parameters:
+  YcorX = cor( y , x ) # correlation of y with each x predictor
+  Rsq = zbeta %*% matrix( YcorX , ncol=1 )
+  #-----------------------------------------------------------------------------
+  if ( pairsPlot ) {
+    # Plot the parameters pairwise, to see correlations:
+    openGraph()
+    nPtToPlot = 1000
+    plotIdx = floor(seq(1,chainLength,by=chainLength/nPtToPlot))
+    panel.cor = function(x, y, digits=2, prefix="", cex.cor, ...) {
+      usr = par("usr"); on.exit(par(usr))
+      par(usr = c(0, 1, 0, 1))
+      r = (cor(x, y))
+      txt = format(c(r, 0.123456789), digits=digits)[1]
+      txt = paste(prefix, txt, sep="")
+      if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+      text(0.5, 0.5, txt, cex=1.25 ) # was cex=cex.cor*r
+    }
+    pairs( cbind( beta0 , beta , sigma )[plotIdx,] ,
+           labels=c( "beta[0]" , 
+                     paste0("beta[",1:ncol(beta),"]\n",xName) , 
+                     expression(sigma) ) , 
+           lower.panel=panel.cor , col="skyblue" )
+    if ( !is.null(saveName) ) {
+      saveGraph( file=paste(saveName,"PostPairs",sep=""), type=saveType)
+    }
+  }
+  #-----------------------------------------------------------------------------
+  # Marginal histograms:
+  
+  decideOpenGraph = function( panelCount , saveName , finished=FALSE , 
+                              nRow=2 , nCol=3 ) {
+    # If finishing a set:
+    if ( finished==TRUE ) {
+      if ( !is.null(saveName) ) {
+        saveGraph( file=paste0(saveName,ceiling((panelCount-1)/(nRow*nCol))), 
+                   type=saveType)
+      }
+      panelCount = 1 # re-set panelCount
+      return(panelCount)
+    } else {
+      # If this is first panel of a graph:
+      if ( ( panelCount %% (nRow*nCol) ) == 1 ) {
+        # If previous graph was open, save previous one:
+        if ( panelCount>1 & !is.null(saveName) ) {
+          saveGraph( file=paste0(saveName,(panelCount%/%(nRow*nCol))), 
+                     type=saveType)
+        }
+        # Open new graph
+        openGraph(width=nCol*7.0/3,height=nRow*2.0)
+        layout( matrix( 1:(nRow*nCol) , nrow=nRow, byrow=TRUE ) )
+        par( mar=c(4,4,2.5,0.5) , mgp=c(2.5,0.7,0) )
+      }
+      # Increment and return panel count:
+      panelCount = panelCount+1
+      return(panelCount)
+    }
+  }
+  
+  # Original scale:
+  panelCount = 1
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+  histInfo = plotPost( beta0 , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(beta[0]) , main="Intercept" )
+  for ( bIdx in 1:ncol(beta) ) {
+    panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+    histInfo = plotPost( beta[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                         xlab=bquote(beta[.(bIdx)]) , main=xName[bIdx] )
+  }
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+  histInfo = plotPost( sigma , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(sigma) , main=paste("Scale") )
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+  histInfo = plotPost( Rsq , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(R^2) , main=paste("Prop Var Accntd") )
+  for ( bIdx in 1:ncol(pred) ) {
+    panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+    histInfo = plotPost( pred[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                         xlab=bquote(pred[.(bIdx)]) , main=paste0("Prediction",bIdx) )
+  }
+  
+  
+  # Standardized scale:
+  panelCount = 1
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  histInfo = plotPost( zbeta0 , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(z*beta[0]) , main="Intercept" )
+  for ( bIdx in 1:ncol(beta) ) {
+    panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+    histInfo = plotPost( zbeta[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                         xlab=bquote(z*beta[.(bIdx)]) , main=xName[bIdx] )
+  }
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  histInfo = plotPost( zsigma , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(z*sigma) , main=paste("Scale") )
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  histInfo = plotPost( Rsq , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(R^2) , main=paste("Prop Var Accntd") )
+  panelCount = decideOpenGraph( panelCount , finished=TRUE , saveName=paste0(saveName,"PostMargZ") )
+  
+  #-----------------------------------------------------------------------------
+}
+#===============================================================================
